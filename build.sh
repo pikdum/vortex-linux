e60b5abbbda7aa6bab1ea9aa564f4903e0371c1a
pikdum <pikdum@kuudere.moe>
2023-05-27 01:32:31 -0500

    feat: .desktop, config

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ rm -rf tmp
 mkdir -p tmp bin
 trap 'rm -rf tmp' EXIT
 
-esbuild main.js --bundle --platform=node --outfile=tmp/main.js
+esbuild main.js --bundle --platform=node --outfile=tmp/main.js --loader:.ico=dataurl --loader:.desktop=text
 node --experimental-sea-config sea-config.json
 cp $(command -v node) bin/vortex-linux
 postject bin/vortex-linux NODE_SEA_BLOB tmp/sea-prep.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2
--- main.js
+++ main.js
@@ -1,7 +1,27 @@
 import { program } from "commander";
 
 import { downloadProton, setProton } from "./lib/proton.js";
-import { downloadVortex, installVortex, launchVortex } from "./lib/vortex.js";
+import {
+  downloadVortex,
+  installVortex,
+  launchVortex,
+  setupVortexDesktop,
+} from "./lib/vortex.js";
+
+import { getConfig, setConfig } from "./lib/config.js";
+
+const originalEmit = process.emit;
+// eslint-disable-next-line
+process.emit = function (name, data, ...args) {
+  if (
+    name === `warning` &&
+    typeof data === `object` &&
+    data.name === `ExperimentalWarning`
+  ) {
+    return false;
+  }
+  return originalEmit.apply(process, arguments);
+};
 
 program.version("0.0.1");
 
@@ -38,6 +58,28 @@ program
   .description("Launch Vortex")
   .action(async (args) => {
     await launchVortex(args);
+  });
+
+program
+  .command("setupVortexDesktop")
+  .description("Setup .desktop entry for Vortex")
+  .action(() => {
+    setupVortexDesktop();
+  });
+
+program
+  .command("getConfig")
+  .description("Output config")
+  .action(() => {
+    console.log(JSON.stringify(getConfig(), null, 2));
+  });
+
+program
+  .command("setConfig <key> [value]")
+  .description("Set config key")
+  .action((key, value) => {
+    setConfig(key, value);
+    console.log(JSON.stringify(getConfig(), null, 2));
   });
 
 program.parse(process.argv);
