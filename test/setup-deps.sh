376137099ff144503e1a84bbc4b42e293f07dc9b
pikdum <pikdum@kuudere.moe>
2023-08-21 03:24:33 -0500

    fix: consolidate into single job

--- test/setup-deps.sh
+++ test/setup-deps.sh
@@ -7,6 +7,6 @@ apt install -y wget curl git python3 build-essential
 dpkg --add-architecture i386
 mkdir -pm755 /etc/apt/keyrings
 wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
-wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
+wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/lunar/winehq-lunar.sources
 apt update
 apt install -y --install-recommends winehq-devel
